6459d8ac3af5e5baba0b7a40ca564aa46288c16e
pedrolorandi <pedrokl@hotmail.com>
2024-05-24 15:49:06 -0400

    fixed save image

--- image_creator.py
+++ image_creator.py
@@ -3,9 +3,14 @@ import openai
 from dotenv import load_dotenv
 from helper import zodiac_signs, clear_and_wait
 
+import base64
+from PIL import Image
+from io import BytesIO
+
 # Load environment variables from .env file
 load_dotenv()
 
+# Set OpenAI API key from environment variables
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def create_image(phrases):
@@ -16,23 +21,33 @@ def create_image(phrases):
   if not os.path.exists(image_folder):
     os.makedirs(image_folder)
 
+  # Iterate through each zodiac sign and their corresponding phrases
   for sign_name in zodiac_signs:
     phrases_number = len(phrases[sign_name]['phrases'])
 
+    # Generate images for each phrase
     for i, phrase in enumerate(phrases[sign_name]['phrases']):
       try:
         print(f"Creating image for {sign_name} ({i + 1}/{phrases_number})")
         response = openai.Image.create(
           model="dall-e-3",
-          # prompt=f"Do not include any text in the image. Create an image for this phrase: '{phrase}' in the highest resolution. The style of the images should be a blend of surrealism and fantasy with elements of digital art. The image should feature a serene and reflective scene with surreal elements. The landscape should have an ethereal quality with vibrant colors and detailed composition. Use atmospheric lighting to create a sense of calm and introspection, incorporating symbolism to convey themes of self-assessment, progress, and personal growth.",
-          prompt=f"Do not include any text in the image. Create an image for this phrase: '{phrase}' in the highest resolution.",
+          prompt=f"Do not include any text in the image. Create an image for this phrase: '{phrase}' in the highest resolution. The style of the images should be a blend of surrealism and fantasy with elements of digital art. The image should feature a serene and reflective scene with surreal elements. The landscape should have an ethereal quality with vibrant colors and detailed composition. Use atmospheric lighting to create a sense of calm and introspection, incorporating symbolism to convey themes of self-assessment, progress, and personal growth.",
           size="1024x1024",
           quality="standard",
+          response_format="b64_json",
+          style="vivid",
           n=1,
         )
 
-        image_url = response['data'][0]['url']
-        print(image_url)
+        # Decode the base64 image data
+        base64_image = response['data'][0]['b64_json']
+        decoded_image = base64.b64decode(base64_image)
+
+        # Open the image using PIL
+        image = Image.open(BytesIO(decoded_image))
+
+        # Save the image to the specified folder
+        image.save(f"{image_folder}/{sign_name}_{i}.jpg")
 
       except Exception as e:
         print(f"Error creating image for {sign_name} ({i + 1}/{phrases_number}): {e}")
