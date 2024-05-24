d21ecf40b79b104c9f96caf586e088f412c44b12
pedrolorandi <pedrokl@hotmail.com>
2024-05-24 15:00:04 -0400

    fix image settings

--- image_creator.py
+++ image_creator.py
@@ -1,9 +1,12 @@
 import os
 import openai
+from dotenv import load_dotenv
 from helper import zodiac_signs, clear_and_wait
 
+# Load environment variables from .env file
+load_dotenv()
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
-client = OpenAI()
 
 def create_image(phrases):
   clear_and_wait()  # Clear console or perform any necessary setup
@@ -17,8 +20,23 @@ def create_image(phrases):
     phrases_number = len(phrases[sign_name]['phrases'])
 
     for i, phrase in enumerate(phrases[sign_name]['phrases']):
-      print(f"Creating image for {sign_name} ({i + 1}/{phrases_number})")
-      # Create image here
+      try:
+        print(f"Creating image for {sign_name} ({i + 1}/{phrases_number})")
+        response = openai.Image.create(
+          model="dall-e-3",
+          # prompt=f"Do not include any text in the image. Create an image for this phrase: '{phrase}' in the highest resolution. The style of the images should be a blend of surrealism and fantasy with elements of digital art. The image should feature a serene and reflective scene with surreal elements. The landscape should have an ethereal quality with vibrant colors and detailed composition. Use atmospheric lighting to create a sense of calm and introspection, incorporating symbolism to convey themes of self-assessment, progress, and personal growth.",
+          prompt=f"Do not include any text in the image. Create an image for this phrase: '{phrase}' in the highest resolution.",
+          size="1024x1024",
+          quality="standard",
+          n=1,
+        )
+
+        image_url = response['data'][0]['url']
+        print(image_url)
+
+      except Exception as e:
+        print(f"Error creating image for {sign_name} ({i + 1}/{phrases_number}): {e}")
+        os.system('sleep 60')
 
   clear_and_wait()  # Clear console or perform any necessary cleanup
   return True
