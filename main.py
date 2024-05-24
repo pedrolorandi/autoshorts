8ba782cac517dc60ff64bade262b5bec9018f656
pedrolorandi <pedrokl@hotmail.com>
2024-05-24 00:07:50 -0400

    add error handler to main and scraper

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ from state import load_state, save_state, clear_state
   
 # Run the scraper
 def main():
-  state = load_state()
-  clear_state()
+  try:
+    state = load_state()
+    clear_state()
 
-  # Scrape horoscope and save the state
-  horoscope = get_horoscope()
-  state['horoscope'] = horoscope
-  save_state(state)
+    # Scrape horoscope and save the state
+    horoscope = get_horoscope()
+    state['horoscope'] = horoscope
+    save_state(state)
 
-  # Create script and save the state
-  script = create_script(state['horoscope'])
-  state['scripts'] = script
-  save_state(state)
+    # Create script and save the state
+    script = create_script(state['horoscope'])
+    state['scripts'] = script
+    save_state(state)
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
   main()
--- scraper.py
+++ scraper.py
@@ -11,17 +11,24 @@ def get_horoscope_from_souce1():
   base_url = 'https://www.horoscope.com/us/horoscopes/general/horoscope-general-daily-today.aspx?sign='
 
   for sign_id in range(1, 13):
-    print(f"Scraping {zodiac_signs[sign_id - 1]} from Horoscope.com")
-    response = requests.get(f"{base_url}{sign_id}")
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # Extract the horoscope text
-    full_text = soup.find('div', class_='content').find_next('p').text.strip()
-    horoscope_text = full_text.split(' - ', 1)[1].strip()
-
-    # Store the horoscope text in a dictionary
     sign_name = zodiac_signs[sign_id - 1]
-    horoscope[sign_name] = horoscope_text
+
+    try:
+      print(f"Scraping {sign_name} from Horoscope.com")
+      response = requests.get(f"{base_url}{sign_id}")
+      response.raise_for_status()
+      soup = BeautifulSoup(response.content, 'html.parser')
+
+      # Extract the horoscope text
+      full_text = soup.find('div', class_='content').find_next('p').text.strip()
+      horoscope_text = full_text.split(' - ', 1)[1].strip()
+
+      # Store the horoscope text in a dictionary
+      horoscope[sign_name] = horoscope_text
+    except Exception as e:
+      print(f"An error occurred while scraping {sign_name} from Horoscope.com: {e}")
+      horoscope[sign_name] = ''
+
   return horoscope
 
 def get_horoscope_from_souce2():
@@ -31,15 +38,21 @@ def get_horoscope_from_souce2():
   base_url = 'https://astrostyle.com/horoscopes/daily/'
 
   for sign_name in zodiac_signs:
-    print(f"Scraping {sign_name} from Astrostyle.com")
-    response = requests.get(f"{base_url}{sign_name.lower()}/")
-    soup = BeautifulSoup(response.content, 'html.parser')
+    try:
+      print(f"Scraping {sign_name} from Astrostyle.com")
+      response = requests.get(f"{base_url}{sign_name.lower()}/")
+      response.raise_for_status()
+      soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract the horoscope text
-    horoscope_text = soup.find('div', class_='horoscope-content', style="display:block;").find_next('p').text.strip()
+      # Extract the horoscope text
+      horoscope_text = soup.find('div', class_='horoscope-content', style="display:block;").find_next('p').text.strip()
 
-    # Store the horoscope text in a dictionary
-    horoscope[sign_name] = horoscope_text
+      # Store the horoscope text in a dictionary
+      horoscope[sign_name] = horoscope_text
+    except Exception as e:
+      print(f"An error occurred while scraping {sign_name} from Astrostyle.com: {e}")
+      horoscope[sign_name] = ''
+
   return horoscope
 
 def get_horoscope():
