ac44446c9d707934a030f8a196c31a0402ec9594
pedrolorandi <pedrokl@hotmail.com>
2024-05-24 01:29:59 -0400

    added create_phrases

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from helper import clear_and_wait
 from scraper import get_horoscope
 from script_creator import create_script
+from phrase_creator import create_phrases
 from state import load_state, save_state, clear_state
   
 # Run the scraper
@@ -18,6 +19,12 @@ def main():
     script = create_script(state['horoscope'])
     state['scripts'] = script
     save_state(state)
+
+    # Create phrases and save the state
+    phrases = create_phrases(state['scripts'])
+    state['phrases'] = phrases
+    save_state(state)
+
   except Exception as e:
     print(f"An error occurred: {e}")
 
