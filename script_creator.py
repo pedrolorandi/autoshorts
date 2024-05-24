3856241830a2bb12e8f67c871d4b0feba3aa55cd
pedrolorandi <pedrokl@hotmail.com>
2024-05-24 15:49:18 -0400

    add comments

--- script_creator.py
+++ script_creator.py
@@ -6,6 +6,7 @@ from helper import zodiac_signs, clear_and_wait
 # Load environment variables from .env file
 load_dotenv()
 
+# Set OpenAI API key from environment variables
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def create_script(horoscope):
@@ -18,7 +19,7 @@ def create_script(horoscope):
       response = openai.ChatCompletion.create(
         model="gpt-4o",
         messages=[
-          {"role": "system", "content": "You are a female professional astrologer. The user will send you some information and your job is to summarize it and rewrite it in a more professional manner. CREATE 6 SENTENCES ONLY. CREATE SENTENCES WITH 10 WORDS ONLY. MAKE SURE TO USE SIMPLE WORDS IN A CLEVER WAY. MAKE THE SCRIPT EASY TO UNDERSTAND. DO NOT ADD ANY NEW INFORMATION. DO NOT ADD NEW LINES. DO NOT ADD SIGNATURES. DO NOT ADD ANY TYPE OF QUOTES. DO NOT ADD SPECIAL CHARACTERS. DO NOT ADD EMOJI. ALWAYS USE THE SIGN NAME IN THE FIRST SENTENCE"},
+          {"role": "system", "content": "You are a female professional astrologer. The user will send you some information and your job is to summarize it and rewrite it in a more professional manner. CREATE 6 SENTENCES ONLY. CREATE SENTENCES WITH 10 WORDS ONLY. ALWAYS USE THE SIGN NAME IN THE FIRST SENTENCE. MAKE SURE TO USE SIMPLE WORDS IN A CLEVER WAY. MAKE THE SCRIPT EASY TO UNDERSTAND. DO NOT ADD ANY NEW INFORMATION. DO NOT ADD NEW LINES. DO NOT ADD SIGNATURES. DO NOT ADD ANY TYPE OF QUOTES. DO NOT ADD SPECIAL CHARACTERS. DO NOT ADD EMOJI."},
           {"role": "user", "content": horoscope[sign_name]['text']}
         ]
       )
