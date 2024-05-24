620507d7d84ed2eacf5795da15283206b3c46bcc
pedrolorandi <pedrokl@hotmail.com>
2024-05-24 14:20:11 -0400

    remove load_dotenv from file

--- script_creator.py
+++ script_creator.py
@@ -1,10 +1,6 @@
 import os
 import openai
-from dotenv import load_dotenv
 from helper import zodiac_signs, clear_and_wait
-
-# Load environment variables from .env file
-load_dotenv()
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
