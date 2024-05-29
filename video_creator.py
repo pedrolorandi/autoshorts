bafccb1cb41b06382477ab629ab1e79a636c2934
pedrolorandi <pedrokl@hotmail.com>
2024-05-29 12:52:10 -0400

    refactor code

--- video_creator.py
+++ video_creator.py
@@ -1,63 +1,67 @@
 import os
 from helper import zodiac_signs, clear_and_wait
-from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips, TextClip, CompositeVideoClip
+from moviepy.editor import ImageClip, AudioFileClip, VideoFileClip, TextClip, concatenate_videoclips, CompositeVideoClip
 from moviepy.video.fx.all import crop
+
+def process_clip(image_path, audio_path):
+  # Process an image and audio file into a video clip
+  audio_clip = AudioFileClip(audio_path)
+  video_duration = audio_clip.duration
+
+  image_clip = ImageClip(image_path).set_duration(video_duration)
+  image_clip = image_clip.resize(lambda t: 1 + (0.047 * t)).set_position(('center'))
+  
+  return CompositeVideoClip([image_clip.set_audio(audio_clip)])
+
+def crop_clip(video_clip, frame_size):
+  # Crop the video clip to the specified frame size
+  (video_clip_width, video_clip_height) = video_clip.size
+  crop_width = video_clip_height * 9/16
+
+  x1, x2 = (video_clip_width - crop_width)//2, (video_clip_width + crop_width)//2
+  y1, y2 = 0, video_clip_height
+
+  return crop(video_clip, x1=x1, y1=y1, x2=x2, y2=y2)
+
+def prepare_particle_clip(particle_video_path, frame_size):
+  # Prepare the particle clip by resizing and cropping
+  particle_clip = VideoFileClip(particle_video_path)
+  particle_clip = particle_clip.resize(width=frame_size[0], height=frame_size[1])
+  return crop_clip(particle_clip, frame_size)
 
 def create_video(phrases):
   clear_and_wait()  # Clear console or perform any necessary setup
 
-  # Verify if folder 'audio' exists, if not, create it
   video_folder = 'video'
-  if not os.path.exists(video_folder):
-    os.makedirs(video_folder)
+  os.makedirs(video_folder, exist_ok=True)
 
-  # Define paths for audio and images
   audio_folder = 'audio'
   image_folder = 'image'
-
-  # Frame size for the video in portrait mode
+  particle_video_path = 'video/Particle.mp4'
   frame_size = (1080, 1920)
 
-  # Collect all image and audio files for the sign
+   # Prepare the particle clip
+  particle_clip = prepare_particle_clip(particle_video_path, frame_size)
+
+  # Process videos for each zodiac sign
   for sign_name in zodiac_signs:
     video_clips = []
-    phrases_number = len(phrases[sign_name]['phrases'])
 
-    for i in range(phrases_number):
+    for i in range(len(phrases[sign_name]['phrases'])):
       image_path = f"{image_folder}/{sign_name}_{i}.jpg"
       audio_path = f"{audio_folder}/{sign_name}_{i}.wav"
-
-      # Get audio duration
-      audio_clip = AudioFileClip(audio_path)
-      video_duration = audio_clip.duration
-
-      # Create an image clip
-      image_clip = ImageClip(image_path).set_duration(video_duration)
-
-      # Moving across the image
-      image_clip = image_clip.resize(lambda t: 1 + (0.047 * t))
-      image_clip = image_clip.set_position(('center'))
-      
-      # Combine the image and audio clips
-      video_clip = CompositeVideoClip([image_clip.set_audio(audio_clip)])
-
-      # Add the video clip to the list
-      video_clips.append(video_clip)
+      video_clips.append(process_clip(image_path, audio_path))
     
-    # Concatenate all video clips for the sign
     concatenated_clip = concatenate_videoclips(video_clips)
+    concatenated_clip = crop_clip(concatenated_clip, frame_size)
 
-    # Calculate the crop dimensions
-    (concatenated_clip_width, concatenated_clip_height) = concatenated_clip.size
-    crop_width = concatenated_clip_height * 9/16
+    # Set the duration of the particle clip to match the concatenated clip
+    particle_clip = particle_clip.set_duration(concatenated_clip.duration)
 
-    x1, x2 = (concatenated_clip_width - crop_width)//2, (concatenated_clip_width + crop_width)//2
-    y1, y2 = 0, concatenated_clip_height
+    # Combine the concatenated clip and particle clip
+    final_clip = CompositeVideoClip([concatenated_clip, particle_clip.set_position('center').set_opacity(0.25)])
 
-    # Crop the video clip to the frame size
-    concatenated_clip = crop(concatenated_clip, x1=x1, y1=y1, x2=x2, y2=y2)
-
-    # Save the concatenated video clip
-    concatenated_clip.write_videofile(f"{video_folder}/{sign_name}.mp4", codec='libx264', fps=24, threads=4)
+    # Save the final video
+    final_clip.write_videofile(f"{video_folder}/{sign_name}.mp4", codec='libx264', fps=24, threads=4)
 
   clear_and_wait()  # Clear console or perform any necessary cleanup
